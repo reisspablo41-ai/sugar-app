9beb4e0c55cfffad1eb4f506a65d1a30619b2052
Hermann Afeseh Buma <156859033+hermannafesehbuma@users.noreply.github.com>
2025-12-10 01:40:02 -0500

    fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Nunito_Sans } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const nunitoSans = Nunito_Sans({
@@ -27,6 +28,24 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${nunitoSans.variable} font-sans antialiased`}>
         {children}
+        {/* Tawk.to Live Chat Script */}
+        <Script
+          id="tawk-to-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/69391185dac5651980f2a071/1jc3eossu';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   );
